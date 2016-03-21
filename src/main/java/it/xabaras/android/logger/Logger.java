d7014dceecf3acdff2e90ad4756beca90003569c
Paolo Montalto <montypablo@gmail.com>
2016-03-21 20:06:58 +0100

    version 1.2: added methods taking Object o, Exception e as parameters

--- src/main/java/it/xabaras/android/logger/Logger.java
+++ src/main/java/it/xabaras/android/logger/Logger.java
@@ -2,6 +2,15 @@ package it.xabaras.android.logger;
 
 import android.util.Log;
 
+/***
+ * Logger v. 1.2
+ * 
+ * This is a simple class wrapping android logger introducing some utilities
+ * 
+ * @author Paolo Montalto
+ *
+ * Copyright (C) 2012 Paolo Montalto <webmaster@xabaras.it>
+ */
 public class Logger {
 	
 	public static void d(String tag, Exception e) {
@@ -12,12 +21,22 @@ public class Logger {
 		Log.d(tag, msg);
 	}
 	
+	public static void d(Object o, Exception e) {
+		String tag = o != null ? o.getClass().getSimpleName() : "";
+		Log.d(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
+	}
+	
 	public static void e(String tag, Exception e) {
 		Log.e(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
 	}
 
 	public static void e(String tag, String msg) {
 		Log.e(tag, msg);
+	}
+	
+	public static void e(Object o, Exception e) {
+		String tag = o != null ? o.getClass().getSimpleName() : "";
+		Log.e(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
 	}
 	
 	public static void i(String tag, Exception e) {
@@ -28,12 +47,22 @@ public class Logger {
 		Log.i(tag, msg);
 	}
 	
+	public static void i(Object o, Exception e) {
+		String tag = o != null ? o.getClass().getSimpleName() : "";
+		Log.i(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
+	}
+	
 	public static void v(String tag, Exception e) {
 		Log.v(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
 	}
 	
 	public static void v(String tag, String msg) {
 		Log.v(tag, msg);
+	}
+	
+	public static void v(Object o, Exception e) {
+		String tag = o != null ? o.getClass().getSimpleName() : "";
+		Log.v(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
 	}
 	
 	public static void w(String tag, Exception e) {
@@ -44,11 +73,21 @@ public class Logger {
 		Log.w(tag, msg);
 	}
 	
+	public static void w(Object o, Exception e) {
+		String tag = o != null ? o.getClass().getSimpleName() : "";
+		Log.w(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
+	}
+	
 	public static void wtf(String tag, Exception e) {
 		Log.wtf(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
 	}
 	
 	public static void wtf(String tag, String msg) {
 		Log.wtf(tag, msg);
+	}
+	
+	public static void wtf(Object o, Exception e) {
+		String tag = o != null ? o.getClass().getSimpleName() : "";
+		Log.wtf(tag, e.getMessage() != null ? e.getMessage() : e.getClass().toString() + ": No message");
 	}
 }
